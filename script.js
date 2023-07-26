4deaeffb3244a9c41bc146e0cc5d6fed8871034c
Tomasz Smolarczyk <smolarczyk.thomas@gmail.com>
2023-07-26 20:10:23 +0200

    ready for deploy

---
+++ script.js
@@ -0,0 +1,28 @@
+const consoleLogElement = document.querySelector(".console-log");
+
+class StringBuilder {
+  constructor(baseString = "") {
+    this.value = baseString;
+  }
+
+  append(str) {
+    this.value += str;
+    return this;
+  }
+
+  prepend(str) {
+    this.value = str + this.value;
+    return this;
+  }
+
+  pad(str) {
+    this.value = str + this.value + str;
+    return this;
+  }
+}
+
+const builder = new StringBuilder(".");
+builder.append("^").prepend("^").pad("=");
+
+console.log(builder.value);
+consoleLogElement.innerHTML = `&nbsp;${builder.value}`;
